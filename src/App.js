174ae7f2832a6570658868f6903f469f02af7177
Beau <beaulazear@gmail.com>
2024-06-08 18:43:15 -0400

    added some more styling, preparing images to become links. hovering over image changes header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import CLOUDS from 'vanta/dist/vanta.clouds.min';
 import img1 from './images/img1.jpeg'; // Replace with your image paths
-import img2 from './images/img2.jpeg';
+import img2 from './images/img2.jpg';
 import img3 from './images/img3.jpg';
 import img4 from './images/img4.jpg';
 
@@ -13,15 +13,67 @@ function App() {
     })
   }, [])
 
+  // State to manage header name
+  const [headerName, setHeaderName] = useState('Christian Beau Lazear');
+
+  const handleMouseEnter = (name) => {
+    setHeaderName(name);
+  }
+
+  const handleClick = (name) => {
+    if (headerName === name) {
+      console.log('link click')
+    } else {
+      setHeaderName(name);
+    }
+  }
+
+  const handleMouseLeave = () => {
+    setHeaderName('Christian Beau Lazear');
+  }
+
   return (
     <div className="App">
       <div className='bg' id='vanta'>
-        <h1 className='header'>Christian Beau Lazear</h1>
+        <h1 className='header'>{headerName}</h1>
         <div className='image-container'>
-          <img src={img1} alt="img1" className="diamond-img" id="top" />
-          <img src={img2} alt="img2" className="diamond-img" id="right" />
-          <img src={img3} alt="img3" className="diamond-img" id="bottom" />
-          <img src={img4} alt="img4" className="diamond-img" id="left" />
+          {/* Images */}
+          <img
+            src={img1}
+            alt="img1"
+            className="diamond-img"
+            id="top"
+            onMouseEnter={() => handleMouseEnter('Image 1')}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => handleClick('Image 1')}
+          />
+          <img
+            src={img2}
+            alt="img2"
+            className="diamond-img"
+            id="right"
+            onMouseEnter={() => handleMouseEnter('Image 2')}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => handleClick('Image 2')}
+          />
+          <img
+            src={img3}
+            alt="img3"
+            className="diamond-img"
+            id="bottom"
+            onMouseEnter={() => handleMouseEnter('Image 3')}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => handleClick('Image 3')}
+          />
+          <img
+            src={img4}
+            alt="img4"
+            className="diamond-img"
+            id="left"
+            onMouseEnter={() => handleMouseEnter('Image 4')}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => handleClick('Image 4')}
+          />
         </div>
       </div>
     </div>
